8928000cf6ccb7e87845f1915a1c9de64c8d9710
Mac <mac@Melvins-iMac.lan>
2023-09-02 15:51:25 -0400

    Added imgs to Login and Register pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,13 @@
 import AuthForm from "./AuthForm";
+import loginImg from "../assets/loginImg.png";
 
 function Login() {
   return (
     <div className="h-[80vh] flex flex-col">
       <AuthForm isLogin={true} />
+      <div className="mt-20 flex justify-center">
+        <img className="w-40" src={loginImg} alt="" />
+      </div>
     </div>
   );
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,13 @@
 import AuthForm from "./AuthForm";
+import registerImg from "../assets/registerImg.png";
 
 function Register() {
   return (
     <div className="h-[80vh] flex flex-col">
       <AuthForm isLogin={false} />
+      <div className="mt-20 flex justify-center">
+        <img className="w-40" src={registerImg} alt="" />
+      </div>
     </div>
   );
 }
