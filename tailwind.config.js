b8fa4f5307bb5c95346f168f61d3fe75bf3a1534
Mac <mac@Melvins-iMac.lan>
2023-09-21 11:04:49 -0400

    testing hosting #3

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,7 @@ export default {
         lobster: ["Lobster", "cursive"],
       },
       backgroundImage: {
-        banner: "url('public/banner-img.jpg')",
+        banner: "url('/banner-img.jpg')",
       },
     },
   },
