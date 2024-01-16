9d8a79c5cb264fe2a00630a98c4d23b635d46dff
Mac <mac@Melvins-iMac.lan>
2024-01-16 16:05:43 -0500

    Modified ProductModal styles to be more responsive on small screens

--- src/components/ProductModal.jsx
+++ src/components/ProductModal.jsx
@@ -27,34 +27,34 @@ const ProductModal = ({ product, closeModal, addToCart }) => {
         className="modal-bg fixed inset-0 bg-black opacity-50 z-40"
         onClick={closeModal}
       />
-      <div className="modal-container bg-white rounded-lg shadow-lg p-8 w-[46rem] h-[60rem] z-50 flex flex-col justify-evenly ">
-        <h1 className="text-2xl font-bold mb-10">
+      <div className="modal-container bg-white rounded-lg shadow-lg p-4 sm:p-8 w-full sm:w-[46rem] h-full sm:h-[60rem] z-50 flex flex-col justify-evenly">
+        <h1 className="text-lg sm:text-2xl font-bold mb-4 sm:mb-10">
           Category: {product.category}
         </h1>
-        <p className="text-xl text-black">
+        <p className="text-base sm:text-xl">
           Rating{" "}
           <span className="text-orange-400 font-semibold">
             {product.rating.rate}
           </span>
         </p>
-        <p className="font-bungee text-orange-400 text-xl text-right">
+        <p className="text-right text-base sm:text-xl text-orange-400">
           FREE SHIPPING
         </p>
         <div className="mb-4">
           <img
             src={product.image}
             alt={product.title}
-            className="w-32 h-32 object-fill mx-auto mb-10"
+            className="w-24 h-24 sm:w-32 sm:h-32 object-fill mx-auto mb-6 sm:mb-10"
           />
           <p className="text-center font-semibold">{product.title}</p>
 
-          <p className="font-arial text-gray-600 text-lg leading-relaxed mt-4 ml-4">
+          <p className="text-gray-600 text-sm sm:text-lg leading-relaxed mt-2 sm:mt-4 ml-2 sm:ml-4">
             {product.description}
           </p>
         </div>
         <p className="text-center text-gray-600">${product.price}</p>
         <div className="flex flex-row justify-center items-center">
-          <label htmlFor="quantity" className="mb-2 mr-2">
+          <label htmlFor="quantity" className="mb-2 mr-2 text-sm sm:text-base">
             Quantity
           </label>
           <input
@@ -70,13 +70,13 @@ const ProductModal = ({ product, closeModal, addToCart }) => {
         <div className="flex justify-center">
           <button
             onClick={() => handleAddToCart(product, quantity)}
-            className="bg-orange-500 hover:bg-orange-600 text-white px-4 py-2 rounded-lg text-2xl"
+            className="bg-orange-500 hover:bg-orange-600 text-white text-base sm:text-xl px-3 sm:px-4 py-1 sm:py-2 rounded"
           >
             Add to Cart
           </button>
         </div>
         {isAddedToCart && (
-          <div className="flex justify-center items-center mt-2 mb-4 bg-green-500 text-white p-2 rounded-md mx-40">
+          <div className="flex justify-center items-center mt-2 mb-4 bg-green-500 text-white p-2 rounded-md mx-20 sm:mx-40">
             Item added to cart
           </div>
         )}
@@ -84,7 +84,7 @@ const ProductModal = ({ product, closeModal, addToCart }) => {
         <div className="flex justify-center mt-4">
           <button
             onClick={closeModal}
-            className="text-white -500 bg-orange-500 p-2 px-4 rounded-md"
+            className="text-white bg-orange-500 p-2 px-3 sm:px-4 rounded-md"
           >
             &#x2715;
           </button>
